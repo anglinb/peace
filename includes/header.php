714ee0f3d22e4e75786f2878d490ee01e80e1d71
Brian <laxnski@gmail.com>
2014-02-28 11:02:58 -0800

    Adds google analytics

--- includes/header.php
+++ includes/header.php
@@ -23,6 +23,16 @@
 	<script type="text/javascript" src="js/jquery.nivo.slider.js"></script>
 	<!-- Badger Stuff -->
 	<script type="text/javascript" src="badger.js"></script>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-46088526-6', 'peaceconferencing.org');
+	  ga('send', 'pageview');
+
+	</script>
 	<link rel="stylesheet" href="badger.css" type="text/css" />
 	<title>Peaceconferencing</title>
 	<script>
